8e62834a88a17fb46e3d93920ac315b4236fb455
JosueRecoba <josue.huer.reco.22@gmail.com>
2024-07-24 16:41:32 -0600

    Creación de Login

---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio de Sesion</title>
+    <link rel="icon" href="assets/img/ICon-Upper.png" type="image/x-icon">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <div class="login-page">
+        <div class="container-log">
+            <div class="blue-section">
+                <img src="assets/img/Logo-UpperLogistics.png" alt="Logo Upper Logistics">
+            </div>
+            <div class="form-section-log">
+                <h2>Inicio de Sesión</h2>
+                <form>
+                    <div class="input-group-log">
+                        <input type="text" placeholder="Nombre de Usuario">
+                    </div>
+                    <div class="input-group-log">
+                        <input type="password" placeholder="Contraseña">
+                    </div>
+                    <button type="submit" class="button-log">Login</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
